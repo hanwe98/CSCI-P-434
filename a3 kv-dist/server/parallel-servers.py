904f5e3d1a1f1e6fda1d28e8337fb19452ca6619
Weifeng Han <jimmy875417424@gmail.com>
2022-04-20 19:00:58 -0400

    buggy state because of occupied port, easy bugs are fixed

--- a3 kv-dist/server/parallel-servers.py
+++ a3 kv-dist/server/parallel-servers.py
@@ -6,21 +6,23 @@ from methods import *
 
 numberOfPorts = 2
 serverPorts = [9889 + n for n in range(numberOfPorts)]
-mode = None
+communicationPorts = [10000 + n for n in range(numberOfPorts)]
+mode = 'eventual'
 
-# TO-DO:
-# 1. sequential
 
 def broadcast(msg):
-    serverName = 'localserver'
+    serverName = 'localhost'
     for serverPort in serverPorts:
         socketToOtherReplica = socket(AF_INET, SOCK_STREAM)
         socketToOtherReplica.connect((serverName,serverPort))
+        print(f"sending {msg} to {serverPort}")
         socketToOtherReplica.send(str.encode(msg))
         socketToOtherReplica.close()
         
 def open_server(index):
     port = serverPorts[index]
+    commPort = communicationPorts[index]
+
     try:
         serverPort = port
         serverSocket = socket(AF_INET,SOCK_STREAM)
@@ -35,6 +37,7 @@ def open_server(index):
     # initialize timeStamp and priority queue for total order broadcast
     timeStamp = [index, 0]
     pqueue = []
+    acknowlegements = {}
 
     # start to receive and process msg from the bound port
     while 1:
@@ -42,6 +45,7 @@ def open_server(index):
 
         while 1:
             text = connectionSocket.recv(1024).decode()
+            print(f"{port} receives: {text}")
             incrementTimeStamp(timeStamp) # receive msg
             msg = eval(text)
             cmd = msg[0]
@@ -68,7 +72,11 @@ def open_server(index):
                 # A broadcastMsg is a ["broadcast", key, val, byte, timeStamp : String]
                 broadcastMsg = ["broadcast", key, val, byte, str(timeStamp), 'message']
                 if mode == 'eventual':
-                    broadcast(broadcastMsg)
+                    # print(f"{port} 1, entered set, eventual mode")
+                    # print(f"sending broadcastMsg : {broadcastMsg}")
+                    print(f"{port} start broadcasting")
+                    broadcast(str(broadcastMsg))
+                    print(f"{port} exit from broadcast")
                 if mode == 'sequential':
                     # blocking broadcast
                     broadcast(broadcastMsg)
@@ -79,23 +87,45 @@ def open_server(index):
 
             if cmd == "broadcast":
                 # handle broadcast messages from other replica
+                # print("-----------------------")
+                # print(f"{port} receives a broadcast")
+                # print(f"received broadcastMsg : {text}")
+                # print("-------------")
+                # print("entered cmd = broadcast")
                 val, byte, ts, type = msg[2:]
-                updateTimeStamp(timeStamp, ts)
+                # print(type)
+                # print(timeStamp)
+                # print(ts)
+                updateTimeStamp(timeStamp, eval(ts))
                 if type == 'message':
+                    # print("entered typed == message")
                     # never pop when receive a message because you haven't got your own acknowledgement
                     s = modify(location, key, val, byte)
-                    # add to priority queue, and 
-                    heappush(pqueue, (timeStamp[1], ))
+                    
+                    # add to priority queue
+                    keyOfHeap = timeStamp[1] * 100 + timeStamp[0]
+                    heappush(pqueue, (keyOfHeap, key))
+
                     # send acknowledgement
                     broadcastMsg = ["broadcast", key, val, byte, str(timeStamp), "acknowledgement"]
                     broadcast(broadcastMsg)
                     
                 if type == 'acknowledgement':
+                    # print(f"entered typed == ack")
+                    # add to acknowlegement dictionary and check if any can be poped
+                    count = None
+                    if key not in acknowlegements:
+                        count = 1
+                    else:
+                        count = acknowlegements.get(key) + 1
+                    acknowlegements.update({key, count})
                     
-                    # add to priority queue, and 
-                    # check if any can be poped
-                    ...
-                
+                    # pop only if the msg in the first in the priority queue and has collected all acknowledgements
+                    firstKey = pqueue[0][1]
+                    if acknowlegements.get(firstKey) == numberOfPorts:
+                        acknowlegements.pop(firstKey)    
+                # There is no need to reply to a broadcast, so skip reply and close the socket        
+                # print(f"{port} exit receiving broadcast")
                 break
 
             connectionSocket.send(str.encode(reply))
@@ -109,5 +139,6 @@ if __name__ == '__main__':
         mode = args.consistency
     else:
         raise Exception(f'{args.consistency} : Not a valid mode. Please choose from eventual or sequential.')
+
     with Pool(processes=numberOfPorts) as pool:
         pool.map(open_server, range(numberOfPorts))
