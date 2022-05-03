f449eb0ab0ad46f5103a39cf8db14d28f43fde1a
Weifeng Han <jimmy875417424@gmail.com>
2022-05-03 19:05:05 -0400

    revised report, fix client.py

--- a3 kv-dist/client/client.py
+++ a3 kv-dist/client/client.py
@@ -1,12 +1,16 @@
 from socket import *
 import sys
 
-def findUntilNextSpace(t):
-    i = 0
-    for i in range(0,len(t)):
-        if t[i] == " ":
-            return (t[0:i], t[i+1:])
-    return (t,'')
+
+def sendmsg(msg):
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((serverName,serverPort))
+    print(f"You are connected to port {serverPort} of the {serverName}")
+
+    clientSocket.send(str.encode(str(msg)))
+    modifiedSentence = clientSocket.recv(1024)
+    print(modifiedSentence.decode())
+    clientSocket.close()
 
 try:
     serverName = sys.argv[1] # "localhost" or '127.0.0.1'
@@ -15,6 +19,12 @@ try:
 except: 
     serverName = "localhost"
     serverPort = 9889
+
+
+sendmsg(msg1)
+sendmsg(msg2)
+
+
 
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
@@ -52,3 +62,4 @@ modifiedSentence = clientSocket.recv(1024)
 print(modifiedSentence.decode())
 
 clientSocket.close()
+
